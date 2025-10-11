897eebd5596f7808276cf8815e9829b764c2d62e
Sanjeev Yadav <sanjeevyadavcr7@gmail.com>
2025-10-11 21:33:11 +0530

    fix(notes-list): conditional rendering

--- react-fundamentals/notes-app/src/components/NoteList.jsx
+++ react-fundamentals/notes-app/src/components/NoteList.jsx
@@ -5,27 +5,38 @@ const NoteList = ({ notes, removeNote }) => {
 
   return (
     <div className='space-y-4'>
-      {notes.map((note) => (
-        <div
-          key={note.id}
-          className='p-4 bg-white rounded-lg shadow-md border-l-4'
-        >
-          <h3 className='text-lg font-bold'>{note.title}</h3>
-          <p className='text-sm text-gray-600'>
-            <strong>Category: </strong> {note.category}
-          </p>
-          <p className='pt-1 text-sm text-gray-600'>
-            <strong>Priority: </strong> {note.priority}
-          </p>
-          <p className='pt-2'>{note.description}</p>
-          <button
-            className="className='w-full bg-purple-500 text-white px-4 py-2 mt-2 rounded-lg cursor-pointer hover:bg-purple-600'"
-            onClick={() => removeNote(note.id)}
+      {notes.map((note) => {
+        const noteBorderColor =
+          note.priority.toLowerCase() === 'high'
+            ? 'border-l-red-500'
+            : note.priority.toLowerCase() === 'medium'
+            ? 'border-l-orange-500'
+            : 'border-l-green-500';
+
+        return (
+          <div
+            key={note.id}
+            className={`p-4 bg-white rounded-lg shadow-md border-l-4 ${
+              noteBorderColor || 'border-l-gray-300'
+            }`}
           >
-            Remove
-          </button>
-        </div>
-      ))}
+            <h3 className='text-lg font-bold'>{note.title}</h3>
+            <p className='text-sm text-gray-600'>
+              <strong>Category: </strong> {note.category}
+            </p>
+            <p className='pt-1 text-sm text-gray-600'>
+              <strong>Priority: </strong> {note.priority}
+            </p>
+            <p className='pt-2'>{note.description}</p>
+            <button
+              className="className='w-full bg-purple-500 text-white px-4 py-2 mt-2 rounded-lg cursor-pointer hover:bg-purple-600'"
+              onClick={() => removeNote(note.id)}
+            >
+              Remove
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 };
